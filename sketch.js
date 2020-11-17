04174bd20e5669cb4e9c7bc5c574109aaf408e75
Samriddhi111 <71590668+Samriddhi111@users.noreply.github.com>
2020-11-17 21:07:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	roof=new roof(width/2,670,width,20);
+	ball=new ball(200,200,70);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  roof.display();
+  ball.display();
+
+  drawSprites();
+ 
+}
+
+
+
